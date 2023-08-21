6332854375666eda8f0a787e0081ebd04b849263
Nihar Hegde <niharhegde163@gmail.com>
2023-08-21 15:12:33 +0530

    added a search box

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Link from "next/link";
+import Search from "./Search";
 
 const Navbar = () => {
   return (
@@ -8,6 +9,7 @@ const Navbar = () => {
         <h1 className="text-3xl font-bold text-white grid place-content-center mb-2 md:mb-0">
           <Link href={"/"}>WikiRocket 🚀</Link>
         </h1>
+        <Search />
       </nav>
     </>
   );
