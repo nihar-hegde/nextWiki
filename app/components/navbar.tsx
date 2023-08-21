ed64bcb4202168b5ea5c920849e09401686f344a
Nihar Hegde <niharhegde163@gmail.com>
2023-08-21 14:50:14 +0530

    added nav bar

---
+++ app/components/navbar.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <>
+      <nav className="bg-slate-600 p-4 flex justify-between flex-col md:flex-row sticky top-0 drop-shadow-xl">
+        <h1 className="text-3xl font-bold text-white grid place-content-center mb-2 md:mb-0">
+          <Link href={"/"}>WikiRocket 🚀</Link>
+        </h1>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
